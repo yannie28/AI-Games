f1cde21905f72d79fde8dbf305dec0f0626d478d
ariannetan <aj.yannie@gmail.com>
2019-10-04 16:16:39 +0800

    Update trialCode.py

--- trialCode.py
+++ trialCode.py
@@ -1,11 +1,128 @@
-# boards = {'1': (0,0), '2': (0,1), '3': (0,2),
-#           '4': (1,0), '5': (1,1), '6': (1,2),
-#           '7': (2,0), '8': (2,1), '9': (2,2)}
-# board = [['X','O','X'], ['O','5','6'], ['O','X','O']]
-# avail = {'4': (1,0), '5': (1,1), '6': (1,2)}
-# COMPUTER = 'X'
-# HUMAN = 'O'
+boards = {'1': (0,0), '2': (0,1), '3': (0,2),
+          '4': (1,0), '5': (1,1), '6': (1,2),
+          '7': (2,0), '8': (2,1), '9': (2,2)}
+board = [['O','2','3'], ['4','5','6'], ['7','O','X']]
+avail = {'2': (0,1), '3': (0,2),
+          '4': (1,0), '5': (1,1), '6': (1,2),
+          '7': (2,0),}
+COM_VALUE = 'X'
+HUMAN_VALUE = 'O'
+COMPUTER = 1
+HUMAN = 0
 
+def VictoryFor(board, player):
+    sign = COM_VALUE if player == COMPUTER else HUMAN
+    victory = False
+    diagonal1 = 0
+    diagonal2 = 0
+    for i in range(3):
+        horizontal = vertical = 0
+        for j in range(3):
+            if board[i][j] == sign:
+                horizontal += 1
+            if board[j][i] == sign:
+                vertical += 1
+            if i == j and board[i][j] == sign:
+                diagonal1 += 1
+            if board[0][2] == board[2][0] == board[1][1] == sign:
+                diagonal2 = 3
+ 
+        if horizontal == 3 or vertical == 3 or diagonal1 == 3 or diagonal2 == 3:
+            victory = True
+            break
+
+    return victory
+
+# def evalFunctions(board, player, depth):
+#     print(player)
+#     if VictoryFor(board, player):
+#         return 1000 - depth
+#     elif VictoryFor(board, not player):
+#         return depth - 1000
+#     else:
+#         return 0
+
+def minimax(board, avail, player):
+    
+    if VictoryFor(board, player):
+        return -10
+    elif VictoryFor(board, not player):
+        return 20
+    elif not avail:
+        return 0
+
+    sign = COM_VALUE if player == COMPUTER else HUMAN
+    moves = {}
+    for n in avail:
+        newboard = board[:] #board = [['1','2','3'], ['4','5','6'], ['7','8','9']]
+        newavail = avail.copy()
+        newboard[boards[n][0]][boards[n][1]] = sign
+        print(newavail)
+        del newavail[n] #delete the picked move in the newavail dictionary
+        
+        if player == COMPUTER:
+            result = minimax(newboard, newavail, HUMAN)
+            moves.update({result: n})
+        else:
+            result = minimax(newboard, newavail, COMPUTER)
+            moves.update({result: n})
+
+    bestMove = ''
+    if player == COMPUTER:
+        bestScore =  float('-inf')
+        for i in moves:
+            if int(i) > bestScore:
+                bestScore = int(i)
+                bestMove = moves[i]
+    else:
+        bestScore = float('inf')
+        for i in moves:
+            if int(i) < bestScore:
+                bestScore = int(i)
+                bestMove = moves[i]
+    
+    return bestMove
+
+print(minimax(board, avail, COMPUTER))
+
+# def generateTree(board, avail, depth, player):
+#     maxValue = float('-inf')
+#     minValue = float('inf')
+
+#     sign = COMPUTER if player == 1 else HUMAN
+#     move = ''
+
+#     if VictoryFor(board, player): return 1000
+#     if VictoryFor(board, not player): return -1000
+
+#     if not avail:
+#         return evalFunctions(board, player, depth)
+
+#     for n in avail:
+#         #generate possible move
+#         newboard = board[:] #board = [['1','2','3'], ['4','5','6'], ['7','8','9']]
+#         newavail = avail.copy()
+#         newboard[boards[n][0]][boards[n][1]] = sign
+#         del newavail[n] #delete the picked move in the newavail dictionary
+#         value = generateTree(newboard,newavail, depth+1, not player)
+
+#         if value > maxValue and player == 1:
+#             maxValue = value
+#             move = n
+#         else:
+#             minValue = value
+
+#     if depth == 0:
+#         print(maxValue)
+#         return move
+
+
+#     if player == 1:
+#         return maxValue
+#     else:
+#         return minValue
+    
+# print("Optimal move", generateTree(board,avail,0,1))
 # def minimax(board, avail, depth, player):
 #     if not depth == 3:
 #         return board
@@ -21,65 +138,3 @@
 #         print(minimax(newboard, newavail, depth-1, not player), depth)
 
 # minimax(board,avail,5,1)
-
-graph1 = {
-    'A' : ['B','C'],
-    'B' : ['D','E'],
-    'C' : ['F','G'],
-    'D' : ['H', 'I', 'J'],
-    'E' : ['K'],
-    'F' : ['L', 'M'],
-    'G' : ['N'],
-    'H' : [],
-    'I' : [],
-    'J' : [],
-    'K' : [],
-    'L' : [],
-    'M' : [],
-    'N' : [],
-}
-# MAX = True
-# MIN = False #not MAX
-COMPUTER = 'X' #1-max
-HUMAN = 'O' #0
-
-
-def evalFunction(node):
-    if node == 'H':
-        return 70
-    elif node == 'I':
-        return 120
-    elif node == 'J':
-        return 130
-    elif node == 'K':
-        return 80
-    elif node == 'L':
-        return 100
-    else:
-        return 180
-
-def dfs(graph, node, player):
-    max = float('-inf')
-    min = float('inf')
-    index = ''
-    if not graph1[node]:
-        return evalFunction(node)
-
-    for n in graph[node]:
-        value = dfs(graph,n, not player)
-    
-        if value > max and player == 1:
-            max = value
-            index = n
-        else:
-            min = value
-    
-    if node == 'A':
-        return index
-
-    if player == 1:
-        return max
-    else:
-        return min
-
-print('optimal move =', dfs(graph1,'A', 1))
