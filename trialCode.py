f3013823e44ce10768ba149da4e6f754e1ba36f2
ariannetan <aj.yannie@gmail.com>
2019-10-09 14:37:46 +0800

    Update trialCode.py

--- trialCode.py
+++ trialCode.py
@@ -1,23 +1,23 @@
-from copy import deepcopy
-import numpy as np
-import math
-boards = {'1': (0,0), '2': (0,1), '3': (0,2),
-          '4': (1,0), '5': (1,1), '6': (1,2),
-          '7': (2,0), '8': (2,1), '9': (2,2)}
+# from copy import deepcopy
+# import numpy as np
+# import math
+# boards = {'1': (0,0), '2': (0,1), '3': (0,2),
+#           '4': (1,0), '5': (1,1), '6': (1,2),
+#           '7': (2,0), '8': (2,1), '9': (2,2)}
 
-board = [['1','2','3'], ['O','X','6'], ['O','8','9']]
-board = np.zeros((3,3))
+# board = [['1','2','3'], ['O','X','6'], ['O','8','9']]
+# board = np.zeros((3,3))
 
-COM_VALUE = 'X'
-HUMAN_VALUE = 'O'
-COMPUTER = 1
-HUMAN = 0
+# COM_VALUE = 'X'
+# HUMAN_VALUE = 'O'
+# COMPUTER = 1
+# HUMAN = 0
 
-for i in range(3):
-    board[1][i] = 10
-print(board)
-a = [int(i) for i in list(board[1,:])]
-print(a)
+# for i in range(3):
+#     board[1][i] = 10
+# print(board)
+# a = [int(i) for i in list(board[1,:])]
+# print(a)
 
 # def VictoryFor(board, player):
 #     sign = COM_VALUE if player == COMPUTER else HUMAN_VALUE
@@ -150,3 +150,86 @@ print(a)
 #         print(minimax(newboard, newavail, depth-1, not player), depth)
 
 # minimax(board,avail,5,1)
+# import numpy as np
+# def createBoard():
+# 	board = np.zeros((6,7))
+# 	return board
+
+# board = createBoard()
+
+# for i in range(6):
+#     for j in range(7):
+#         board[i][j] = i+j
+# print(board)
+
+# WINNER = 4
+# for c in range(7): #check for vertical winner
+#     end = WINNER
+#     for r in range(6-3):
+#         print(list(board[r:end, c]))
+#         end+=1
+
+# print(end)
+
+# end = 4
+# for c in range(7-3): #check for horizontal winner
+#     for r in range(6):
+#         print(list(board[r,c:end]))
+#     end+=1
+
+# i = 0
+# while True:
+#     if i == 2:
+#         result = 10
+#     elif i == 5:
+#         print(result)
+#         break
+#     i+=1
+
+import pygame
+import sys
+
+# --- constants ---
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+
+# --- main ---
+
+def main():
+    pygame.init()
+
+    fenetre = pygame.display.set_mode((500, 400), 0, 32)
+
+    current_time = pygame.time.get_ticks()
+
+    delay = 500 # 500ms = 0.5s
+
+    change_time = current_time + delay
+    show = True
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+        fenetre.fill(BLACK)
+        # --- updates ---
+
+        current_time = pygame.time.get_ticks()
+
+        # is time to change ?
+        if current_time >= change_time:
+            # time of next change 
+            change_time = current_time + delay
+            show = not show
+
+        # --- draws ---
+
+        
+
+        if show:
+            pygame.draw.rect(fenetre, WHITE,(200,150,100,50))
+
+        pygame.display.update()
+
+main()
