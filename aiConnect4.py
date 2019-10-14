d8dbd099f85092448de5eb1d7ea78d8e424117b6
ariannetan <aj.yannie@gmail.com>
2019-10-14 10:16:39 +0800

    Update aiConnect4.py

--- aiConnect4.py
+++ aiConnect4.py
@@ -59,7 +59,6 @@ def getWinningMove(winningMove):
         pyg.display.update()
         i += 1
         
-
 def isVictory(board, piece): #check if the player is already a winner
     for c in range(COLUMN): #check for vertical winner
         for r in range(ROW-excess):
@@ -107,114 +106,93 @@ def getValidColumns(board): #return a list of available columns
 def placePiece(board, row, column, piece): #update board
 	board[row][column] = piece
 
-def getHumanMove(board): #get human move
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            sys.exit(0)
-        elif event.type == pyg.MOUSEMOTION:
-            pyg.draw.rect(screen, TEXTAREA, (0,0, COLUMN * BOARDSIZE, BOARDSIZE))
-            pyg.draw.circle(screen, HUMAN_PIECE_COLOR, (event.pos[0], int(BOARDSIZE/2)), RADIUS)
+def getHumanMove(board, move): #get human move
+	col = int(math.floor(move/BOARDSIZE))
 
-        pyg.display.update()
-
-        if event.type == pyg.MOUSEBUTTONDOWN:
-            pyg.draw.rect(screen, TEXTAREA, (0,0, COLUMN * BOARDSIZE, BOARDSIZE))
-            col = int(math.floor(event.pos[0]/BOARDSIZE))
-            if isValidLocation(board, col):
-                row = getAboveRow(board, col)
-                placePiece(board, row, col, HUMAN_VAL)
-                drawMove(board, row, col, HUMAN_VAL)
-
-                return True
+	if isValidLocation(board, col):
+		row = getAboveRow(board, col)
+		placePiece(board, row, col, HUMAN_VAL)
+		drawMove(board, row, col, HUMAN_VAL)
 
 def getAIMove(board): #get ai move
-    col = minimax(board, 5, -math.inf, math.inf, True)[0]
+	col = minimax(board, 5, float('-inf'), float('inf'), True)[0]
 
-    if isValidLocation(board,col):
-        row = getAboveRow(board, col)
-        placePiece(board, row, col, AI_VAL)
-        drawMove(board, row, col, AI_VAL)
+	if isValidLocation(board,col):
+		row = getAboveRow(board, col)
+		placePiece(board, row, col, AI_VAL)
+		drawMove(board, row, col, AI_VAL)
 
-    return col
-
-def evaluate_window(window, piece):
+def countScore(subPart, piece):
 	score = 0
-	opp_piece = HUMAN_VAL
+	oppPiece = HUMAN_VAL
 	if piece == HUMAN_VAL:
-		opp_piece = AI_VAL
+		oppPiece = AI_VAL
 
-	if window.count(piece) == 4:
-		score += 100
-	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-		score += 5
-	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+	if subPart.count(piece) == 4:
+		score += 4
+	elif subPart.count(piece) == 3 and subPart.count(EMPTY) == 1:
+		score += 3
+	elif subPart.count(piece) == 2 and subPart.count(EMPTY) == 2:
 		score += 2
+	else:
+		score += 1
 
-	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
-		score -= 4
+	if subPart.count(oppPiece) == 3 and subPart.count(EMPTY) == 1:
+		score -= 3
 
 	return score
 
 def score_position(board, piece):
 	score = 0
 
-	## Score center column
-	center_array = [int(i) for i in list(board[:, COLUMN//2])]
+	center_array = [int(i) for i in list(board[:, COLUMN//2])] #center column
 	center_count = center_array.count(piece)
 	score += center_count * 3
 
-	## Score Horizontal
-	for r in range(ROW):
-		row_array = [int(i) for i in list(board[r,:])]
+	for r in range(ROW): #horizontal
+		row_array = [int(i) for i in list(board[r,:])] 
 		for c in range(COLUMN-3):
 			window = row_array[c:c+WINNER]
-			score += evaluate_window(window, piece)
+			score += countScore(window, piece)
 
-	## Score Vertical
-	for c in range(COLUMN):
+	for c in range(COLUMN): #vertical
 		col_array = [int(i) for i in list(board[:,c])]
 		for r in range(ROW-3):
 			window = col_array[r:r+WINNER]
-			score += evaluate_window(window, piece)
+			score += countScore(window, piece)
 
-	## Score positive sloped diagonal
-	for r in range(ROW-3):
+	for r in range(ROW-3): #positive diagonal
 		for c in range(COLUMN-3):
 			window = [board[r+i][c+i] for i in range(WINNER)]
-			score += evaluate_window(window, piece)
+			score += countScore(window, piece)
 
-	for r in range(ROW-3):
+	for r in range(ROW-3): #negative diagonal
 		for c in range(COLUMN-3):
 			window = [board[r+3-i][c+i] for i in range(WINNER)]
-			score += evaluate_window(window, piece)
+			score += countScore(window, piece)
 
 	return score
 
-def is_terminal_node(board):
-	return isVictory(board, HUMAN_VAL) or isVictory(board, AI_VAL) or len(getValidColumns(board)) == 0
-
 def minimax(board, depth, alpha, beta, maximizingPlayer):
 	valid_locations = getValidColumns(board)
-	is_terminal = len(getValidColumns(board)) == 0
-	if depth == 0 or is_terminal:
-		if is_terminal:
-			if isVictory(board, AI_VAL):
-				return (None, 100000000000000)
-			elif isVictory(board, HUMAN_VAL):
-				return (None, -10000000000000)
-			else: # Game is over, no more valid moves
-				return (None, 0)
-		else: # Depth is zero
-			return (None, score_position(board, AI_VAL))
+
+	if isVictory(board, AI_VAL):
+		return (None, 100)
+	elif isVictory(board, HUMAN_VAL):
+		return (None, -100)
+	elif depth == 0:
+		return (None, score_position(board, AI_VAL))
+	elif len(getValidColumns(board)) == 0:
+		return (None, 0)
 
 	if maximizingPlayer:
-		value = -math.inf
-		column = random.choice(valid_locations)
+		value = float('-inf')
+		column = ''
 		for col in valid_locations:
 			row = getAboveRow(board, col)
-			b_copy = board.copy()
-			placePiece(b_copy, row, col, AI_VAL)
-			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+			newBoard = board.copy()
+			placePiece(newBoard, row, col, AI_VAL)
+			new_score = minimax(newBoard, depth-1, alpha, beta, False)[1]
 			if new_score > value:
 				value = new_score
 				column = col
@@ -224,13 +202,13 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 		return column, value
 
 	else: # Minimizing player
-		value = math.inf
-		column = random.choice(valid_locations)
+		value = float('inf')
+		column = ''
 		for col in valid_locations:
 			row = getAboveRow(board, col)
-			b_copy = board.copy()
-			placePiece(b_copy, row, col, HUMAN_VAL)
-			new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+			newBoard = board.copy()
+			placePiece(newBoard, row, col, HUMAN_VAL)
+			new_score = minimax(newBoard, depth-1, alpha, beta, True)[1]
 			if new_score < value:
 				value = new_score
 				column = col
@@ -240,36 +218,47 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
 		return column, value
 
 def main():
-    board = np.zeros((ROW,COLUMN))
-    pyg.init()
+	board = np.zeros((ROW,COLUMN))
+	pyg.init()
 
-    drawBoard(board)
-    pyg.display.update()
+	drawBoard(board)
+	pyg.display.update()
 
-    victory = False
-    player = HUMAN
+	victory = False
+	player = HUMAN
+	i = 0
+	while not victory:
+		for event in pyg.event.get():
+			if event.type == pyg.QUIT:
+				sys.exit(0)
+			elif event.type == pyg.MOUSEMOTION:
+				pyg.draw.rect(screen, TEXTAREA, (0,0, COLUMN * BOARDSIZE, BOARDSIZE))
+				pyg.draw.circle(screen, HUMAN_PIECE_COLOR, (event.pos[0], int(BOARDSIZE/2)), RADIUS)
 
-    while not victory:
-        if player == HUMAN:
-            getHumanMove(board)
-            result = isVictory(board, HUMAN_VAL)
-            if result:
-                message = "Finally! You win!"
-                victory = True
-            pyg.display.update()
-            player = AI
+			pyg.display.update()
 
-        elif player == AI and getHumanMove(board):				
-            getAIMove(board)
-            result = isVictory(board, AI_VAL)
-            if result:
-                message = 'Computer Wins!'
-                victory = True
-            player = HUMAN
+			if event.type == pyg.MOUSEBUTTONDOWN:
+				pyg.draw.rect(screen, TEXTAREA, (0,0, COLUMN * BOARDSIZE, BOARDSIZE))
 
-        if victory:
-            getWinningMessage(message)
-            getWinningMove(result)
-            pyg.time.wait(5000)
+				if player == HUMAN:
+					getHumanMove(board, event.pos[0])
+					result = isVictory(board, HUMAN_VAL)
+					if result:
+						message = 'Finally! You win!'
+						victory = True
+					player = AI
+
+		if player == AI and not victory:				
+			getAIMove(board)
+			result = isVictory(board, AI_VAL)
+			if result:
+				message = 'Computer Wins!'
+				victory = True
+			player = HUMAN
+
+		if victory:
+			getWinningMessage(message)
+			getWinningMove(result)
+			pyg.time.wait(5000)
 
 main()
